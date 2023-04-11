b25e1e18b3a984354024c60b8133fbb5ae508ed4
wxnacy <371032668@qq.com>
2023-04-11 17:31:50 +0800

    feat(constants): add var: LoggerPath

--- godler/constants.go
+++ godler/constants.go
@@ -3,6 +3,7 @@ package godler
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
 )
@@ -10,6 +11,16 @@ import (
 const (
 	DOWNLOAD_DIR string = "~/Downloads"
 )
+
+var (
+	LoggerPath string
+)
+
+func init() {
+	cacheDir, _ := homedir.Expand("~/.cache/dler")
+	DirExistsOrCreate(cacheDir)
+	LoggerPath = filepath.Join(cacheDir, "dler.log")
+}
 
 func GetDefaultDownloadDir() string {
 	path, err := homedir.Expand(DOWNLOAD_DIR)
